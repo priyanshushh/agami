feb4a0350293b0ce6a56071824d2e0f7781f9f24
Priyanshu Sharma <priyanshusharmaaug03@gmail.com>
2023-01-03 16:48:43 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -6,12 +6,8 @@ const cors = require("cors");
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const corsOptions = {
-  origin: "https://agami-production.up.railway.app/api/v1/",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+
+app.use(cors());
 const employe = require("./routes/employeeRoutes");
 app.use("/api/v1", employe);
 module.exports = app;
